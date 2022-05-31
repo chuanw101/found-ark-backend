e2b09d0ed6da84e51e685f5ed7ff4ba70642e2e5
Chuan Wang <chuan.wang101@gmail.com>
2022-05-31 14:21:15 -0700

    character routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,4 +3,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 router.use('/users', userRoutes);
 
+const characterRoutes = require('./characterRoutes');
+router.use('/characters', characterRoutes);
+
 module.exports = router;
