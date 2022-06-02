74b736c60050cbabde8dffd3aed83cc7eb7f0886
Chuan Wang <chuan.wang101@gmail.com>
2022-06-01 17:44:46 -0700

    remove stuff related to session

--- server.js
+++ server.js
@@ -1,31 +1,16 @@
 const express = require('express');
-const session = require('express-session');
 const cors = require('cors');
 const routes = require('./routes');
 require("dotenv").config();
 
 const sequelize = require('./config/connection');
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const sess = {
-  secret: process.env.SESSION_SECRET,
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
-
-app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
