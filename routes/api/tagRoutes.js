9e3360b082a2802ad316e78de1a2e841448e26b0
Chuan Wang <chuan.wang101@gmail.com>
2022-06-10 11:37:11 -0700

    comment out bad route

--- routes/api/tagRoutes.js
+++ routes/api/tagRoutes.js
@@ -4,7 +4,12 @@ const { Tag, Group } = require('../../models');
 //find all
 router.get("/", async (req, res) => {
     try {
-        const tags = await Tag.findAll();
+        const tags = await Tag.findAll({
+            include:[{
+                model: Group,
+                attributes:['group_name'],
+            }],
+        });
         res.json(tags);
     }
     catch (err) {
@@ -13,26 +18,23 @@ router.get("/", async (req, res) => {
 });
 
 //find top tags
-router.get("/top/:limit", async (req, res) => {
-    try {
-        const tags = await Tag.findAll({
-            limit: req.params.limit,
-            include:[{
-                model: Group,
-                attributes:[
-                    [Sequelize.fn('COUNT', '*'), 'num_of_group']
-                ]
-            }],
-            order: [
-                ['num_of_group']
-            ]
-        });
-        res.json(tags);
-    }
-    catch (err) {
-        res.status(500).json({ msg: "an error occured", err });
-    }
-});
+// router.get("/top5", async (req, res) => {
+//     try {
+//         const tags = await Tag.findAll({
+//             attributes: { 
+//                 include: [[Sequelize.fn('COUNT', '*'), 'groupCount']] 
+//             },
+//             include:[{
+//                 model: Group,
+                
+//             }],
+//         });
+//         res.json(tags);
+//     }
+//     catch (err) {
+//         res.status(500).json({ msg: "an error occured", err });
+//     }
+// });
 
 
 module.exports = router;
